af94ae10d97141783a9b709a2c843111309e9d14
Guillermo Barila <gui365@yahoo.com.ar>
2018-08-18 18:01:02 -0400

    Working on CSS

--- db/schema.sql
+++ db/schema.sql
@@ -3,7 +3,7 @@ CREATE database burger_db;
 USE burger_db;
 
 CREATE TABLE menu (
-  id AUTO_INCREMENT INT NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
   name VARCHAR (255) NULL,
   eaten BOOLEAN DEFAULT false,
   PRIMARY KEY (id)
